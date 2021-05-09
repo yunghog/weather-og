f95f40790325204629a41742c217543bafef55a9
Samartha <samarthahm@gmail.com>
2021-05-09 17:45:54 +0530

    added linkedin url

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import '../style/style.css';
 function Footer() {
   return (
     <div className="my-footer">
-      <p>Developed by <a href="https://instagram.com/samartha__"  target="_blank" rel="noreferrer">Samartha</a>.
+      <p>Developed by <a href="https://www.linkedin.com/in/samartha-hm-7398861a1/"  target="_blank" rel="noreferrer">Samartha</a>.
         Powered by <a href="https://openweathermap.org/"  target="_blank" rel="noreferrer">Open Weather</a>
       </p>
     </div>
